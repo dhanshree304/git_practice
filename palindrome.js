18f139f8ef9bba3946f12e250f226041daa4bb7c
dhanshree304 <dhanshreeupadhye0@gmail.com>
2023-01-04 00:05:57 +0530

    2 program

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+function checkPalindrome(N, str) {
+    //write code here
+    let bag=""
+    for(let i=N-1;i>=0;i--){
+        bag+=str[i];
+    
+    }
+    
+  
+}
---
+++ prime_num.js
@@ -0,0 +1,10 @@
+function identifyPrime(num) {
+ 
+let c=0;
+for(let i=2;i<=num/2;i++){
+  if(num%i==0){
+      c++
+  }  
+}
+
+}
